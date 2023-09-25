4449aebad3aa4327bbe241d4919662b273bd9ec6
Helga Sallaku <138977903+HelgaSallaku@users.noreply.github.com>
2023-09-24 21:41:09 -0700

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -13,12 +13,10 @@ const busStops = [
     [-71.117585, 42.373016],
     [-71.118625, 42.374863],
   ];
-  
-  // TODO: add your own access token
+
   mapboxgl.accessToken =
     'pk.eyJ1IjoiaHNhbGxha3UiLCJhIjoiY2xteTRyMDZmMTVyZzJtbXpyNDc1MDhxMiJ9.ATILmVZW6DMWj1q7LEcR9Q';
   
-  // This is the map instance
   let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -26,10 +24,8 @@ const busStops = [
     zoom: 14,
   });
   
-  // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
   let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
   
-  // counter here represents the index of the current bus stop
   let counter = 0;
   function move() {
     // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
@@ -43,7 +39,6 @@ const busStops = [
     }, 1000);
   }
   
-  // Do not edit code past this point
   if (typeof module !== 'undefined') {
     module.exports = { move, counter, marker, busStops };
   }
